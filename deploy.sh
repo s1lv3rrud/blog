00aea94e7d041d67a772623bdd05d17ad4d0c022
s1lv3rrud <lek8575@naver.com>
2024-11-18 19:37:25 +0900

    chore: zh-cn 제거

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 # If a command fails then the deploy stops
 set -e
+
+# Print a status message
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-# Build the project.
-####### 이부분을 본인 테마로 변경해주세요~ #######
+
+# Build the Hugo project
+####### Update with your theme if necessary #######
 hugo -t hugo-theme-stack
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
+
+# Check if Hugo build was successful
+if [ $? -ne 0 ]; then
+  echo "Hugo build failed. Exiting deployment."
+  exit 1
 fi
-git commit -m "$msg"
-# Push source and build repos.
-git push origin main
-cd ..
-# anyblogname 업데이트
+
+# Add and commit changes in the main repository (blog content and config updates)
 git add .
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+msg="Rebuilding site $(date)"
+if [ $# -eq 1 ]; then
+  msg="$1"
 fi
 git commit -m "$msg"
 git push origin main
+
+# No need to manually push to the public folder; GitHub Actions will handle deployment
+printf "\033[0;32mDeployment to GitHub complete. GitHub Actions will handle the deployment to Pages.\033[0m\n"
